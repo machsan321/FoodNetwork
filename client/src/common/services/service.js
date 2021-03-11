7a500ca74b3431940300ef43679b5ba10b4aee85
RonZamir <ronzamiron@gmail.com>
2021-03-11 20:46:04 +0200

    dd

--- client/src/common/services/service.js
+++ client/src/common/services/service.js
@@ -1,11 +1,6 @@
 import axios from 'axios'
-
 const baseUrl = "http://localhost:5000/";
-
-
-
 export const PostBaseUrl = async (url, body) => {
-    
     try {
         var res = await axios.post(baseUrl + url, body)
         console.log(res);
